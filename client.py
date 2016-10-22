79033fb81cbbd56d0955b992c7b4b7aad0662c83
gegonmaz <gegonmaz@gmail.com>
2016-10-22 14:04:40 +0200

    modificacion cliente por que salta un error al final

--- client.py
+++ client.py
@@ -8,16 +8,24 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
+#Este primer valor cogera la dir.IP
 SERVER = str(sys.argv[1])
-PORT = int(sys.argv[2])
+#este valor tartara de coger un PUERTO accesible
+try:
+    PORT = int(sys.argv[2])
+except ValueError:
+    print('Error en IP PUERTO')
+#Aqui enviaremos texto (mensaje)
 LINE = str(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
+    print("Enviando: " +  LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
-
-print("Socket terminado.")
+    print("Socket terminado...")
+# Cerramos todo
+my_socket.close()
+print("Proceso Finalizado.")
