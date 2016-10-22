e768183655eb66ae9a73b735330331a05b03f425
gegonmaz <gegonmaz@gmail.com>
2016-10-22 17:48:52 +0200

    ejercicio servidor que lea dir IP y PUERTO del cliente. Solucionar error de sintaxis del print de la linea 31.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys #este comando nos servira para capturar los datos de la shell
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -14,13 +14,23 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
+        IP = str(self.client_adrress[0])
+        print('Dir. IP del cliente: ' + IP)
+        PUERTO = int(self.client_address[1])
+        print('Puerto donde escucha cliente: ' + PUERTO)
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    """
+    Tendremos que cambiar la linea siguiente para que coja el puerto y la dir.IP
+    """
+    #serv = socketserver.UDPServer(('', 6001), EchoHandler)
     try:
-        serv.serve_forever()
+        serv = socketserver.UDPServer(('',int(sys.argv[1]), EchoHandler)
+        print('Lanzando servido UDP de eco...')        
+        serv.serve_forever()    
+    except ValueError:
+        print('PUERTO NO ENCONTRADO')
     except KeyboardInterrupt:
         print("Finalizado servidor")
