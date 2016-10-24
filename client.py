e9e2be1d79a0701f661a8c0daead004439d8c4e3
Gema Gonzalez Mazarias <gegonmaz@alpha07.aulas.gsyc.urjc.es>
2016-10-24 12:44:35 +0200

    ejercicio 6. registro del cliente

--- client.py
+++ client.py
@@ -10,22 +10,28 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 #Este primer valor cogera la dir.IP
 SERVER = str(sys.argv[1])
-#este valor tartara de coger un PUERTO accesible
+#este valor tratara de coger un PUERTO accesible
 try:
     PORT = int(sys.argv[2])
 except ValueError:
     print('Error en IP PUERTO')
-#Aqui enviaremos texto (mensaje)
-LINE = str(sys.argv[3:])
-
+#Aqui enviaremos texto (mensaje)--> join. nos divide el diccionario por ' '.
+if str(sys.argv[3]) == 'register':
+    nombre_usuario_sip =sys.argv[4]
+    print('BIENVENIDO ' + nombre_usuario_sip)
+LINE = ' '.join(sys.argv[3:])
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando: " +  LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    """
+    Aqui le enviará al servidor el mensaje, y el servidor ya se encargara de 
+    ver si es un mensaje al chat, o si es una peticion de registro.
+    """
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
     print("Socket terminado...")
-# Cerramos todo
+# Cerramos todo--> siempre hay que cerrar todos los sockets
 my_socket.close()
 print("Proceso Finalizado.")
